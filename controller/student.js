123ae3cc35834337f388e3051405bfd85ea7e7ac
Guillou Quentin <quentinguillou91@gmail.com>
2023-01-03 11:47:43 +0100

    hotfix - salage + cannot send headers error + secu

--- controller/student.js
+++ controller/student.js
@@ -4,83 +4,84 @@ const bcrypt = require('bcryptjs');
 const config = require('./config');
 
 function loginStudent(req, res) {
-    if(!req.body.username || !req.body.password) {
-        res.status(400).send({ message: "Veuillez remplir tous les champs" });
-        return;
+    if (!req.body.username || !req.body.password) {
+        return res.status(400).send({ message: "Veuillez remplir tous les champs" });
     }
     let username = req.body.username;
     let password = req.body.password;
 
     Student.findOne({ username: username }, (err, student) => {
-        if (err) { res.send(err) }
-        if(!student) { res.send("Student not found") }
+        if (err) { return res.send(err) }
+        if (!student) { return res.send("Student not found") }
         bcrypt.compare(password, student.password, (err, result) => {
             if (err) { res.send(err) }
             if (!result) { res.send("Wrong password") }
             let token = jwt.sign({ id: student._id }, config.secret, {
                 expiresIn: 86400 // expires in 24 hours
-              });
-              res.status(200).send({ auth: "student", token: token});
+            });
+            return res.status(200).send({ auth: "student", token: token });
         })
     })
 }
+
 function getStudentById(req, res) {
     let id = req.params.id;
-    if(!id) { res.status(400).send({ message: "Veuillez remplir tous les champs" }); return; }
+    if (!id) { return res.status(400).send({ message: "Veuillez remplir tous les champs" }); return; }
     Student.findOne({ _id: id }, (err, student) => {
-        if (err) { res.send(err) }
-        if(!student) { res.status(500).send("Student not found") }
+        if (err) { return res.send(err) }
+        if (!student) { return res.status(500).send("Student not found") }
         student.password = bcrypt.decodeBase64(student.password, 24);
-        res.status(200).send({ student: student});
+        return res.status(200).send({ student: student });
     })
 }
 
 function getStudentByToken(req, res) {
     var token = req.headers['x-access-token'];
-    if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' } );
-    else
-    {
+    if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
+    else {
         jwt.verify(token, config.secret, function(err, decoded) {
             if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
             else {
                 let id = decoded.id;
                 Student.findOne({ _id: id }, (err, student) => {
-                    if (err) { res.send(err); return;}
-                    if(!student) { res.status(500).send("Student not found"); return; }
+                    if (err) { res.send(err); return; }
+                    if (!student) { res.status(500).send("Student not found"); return; }
                     student.password = bcrypt.decodeBase64(student.password, 24);
-                    res.status(200).send({ student: student});
+                    res.status(200).send({ student: student });
                 })
             }
             res.status(200).send(decoded);
-          });
+        });
     }
 }
 
 
 
 function registerStudent(req, res) {
-    if(!req.body.username || !req.body.password || !req.body.email || !req.body.prenom || !req.body.nom) {
-        res.status(400).send({ message: "Veuillez remplir tous les champs" });
-        return;
+    console.log("DANS LE REGISTER STUDENT")
+    console.log(!req.body.username || !req.body.password || !req.body.email || !req.body.prenom || !req.body.nom)
+    if (!req.body.username || !req.body.password || !req.body.email || !req.body.prenom || !req.body.nom) {
+        return res.status(400).send({ message: "Veuillez remplir tous les champs" });
     }
-    const hashedPassword = bcrypt.hashSync(req.body.password, 24);
+    console.log("apres if")
+    const hashedPassword = bcrypt.hashSync(req.body.password, 8);
+    console.log("apres hash")
     Student.create({
-        username : req.body.username,
-        password : hashedPassword,
-        email : req.body.email,
-        prenom : req.body.prenom,
-        nom : req.body.nom
+            username: req.body.username,
+            password: hashedPassword,
+            email: req.body.email,
+            prenom: req.body.prenom,
+            nom: req.body.nom
         },
-        function (err, student) {
-        if (err) return res.status(500).send("There was a problem registering the student.")
-
-        console.log("Création d'un student effectué :");
-        console.log(student)
-        let token = jwt.sign({ id: student._id }, config.secret, {
-            expiresIn: 86400 // expires in 24 hours
+        function(err, student) {
+            if (err) return res.status(500).send("There was a problem registering the student.")
+            console.log("Création d'un student effectué :");
+            console.log(student)
+            let token = jwt.sign({ id: student._id }, config.secret, {
+                expiresIn: 86400 // expires in 24 hours
             });
-            res.status(200).send({ auth: "student", token: token});
-    });
+            return res.status(200).send({ auth: "student", token: token });
+        });
 }
 
 module.exports = { loginStudent, registerStudent, getStudentById, getStudentByToken };
--- controller/teacher.js
+++ controller/teacher.js
@@ -4,82 +4,80 @@ const bcrypt = require('bcryptjs');
 const config = require('./config');
 
 function loginTeacher(req, res) {
-    if(!req.body.username || !req.body.password)  return res.status(400).send({ message: "Veuillez remplir tous les champs" });
+    if (!req.body.username || !req.body.password) return res.status(400).send({ message: "Veuillez remplir tous les champs" });
 
     let username = req.params.username;
     let password = req.params.password;
     Teacher.findOne({ username: username }, (err, teacher) => {
-        if (err) { res.send(err) }
-        if(!teacher) { res.send("Teacher not found") }
+        if (err) { return res.send(err) }
+        if (!teacher) { return res.send("Teacher not found") }
         bcrypt.compare(password, student.password, (err, result) => {
-            if (err) { res.send(err) }
-            if (!result) { res.send("Wrong password") }
+            if (err) { return res.send(err) }
+            if (!result) { return res.send("Wrong password") }
             let token = jwt.sign({ id: student._id }, config.secret, {
                 expiresIn: 86400 // expires in 24 hours
-              });
-              res.status(200).send({ auth: "teacher", token: token});
+            });
+            return res.status(200).send({ auth: "teacher", token: token });
         })
-       
+
     })
 }
 
 function getTeacherById(req, res) {
     let id = req.params.id;
-    if(!id)  return res.status(400).send({ message: "Veuillez insérer une id" });
+    if (!id) return res.status(400).send({ message: "Veuillez insérer une id" });
     Teacher.findOne({ _id: id }, (err, teacher) => {
         if (err) return res.send(err);
-        if(!teacher) return res.status(500).send("Teacher not found");
+        if (!teacher) return res.status(500).send("Teacher not found");
         teacher.password = bcrypt.decodeBase64(teacher.password, 24);
-        res.status(200).send({ teacher: teacher});
+        return res.status(200).send({ teacher: teacher });
     })
 }
 
 function getTeacherByToken(req, res) {
     var token = req.headers['x-access-token'];
-    if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' } );
-    else
-    {
+    if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
+    else {
         jwt.verify(token, config.secret, function(err, decoded) {
             if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
-                let id = decoded.id;
-                Teacher.findOne({ _id: id }, (err, teacher) => {
-                    if (err) { res.send(err); return;}
-                    if(!teacher) return res.status(500).send("Teacher not found"); 
-                    teacher.password = bcrypt.decodeBase64(teacher.password, 24);
-                    res.status(200).send({ teacher: teacher});
-                })
-          });
+            let id = decoded.id;
+            Teacher.findOne({ _id: id }, (err, teacher) => {
+                if (err) { return res.send(err); }
+                if (!teacher) return res.status(500).send("Teacher not found");
+                teacher.password = bcrypt.decodeBase64(teacher.password, 24);
+                return res.status(200).send({ teacher: teacher });
+            })
+        });
     }
 }
 
 async function registerTeacher(req, res) {
-   
-    if(!req.body.username || !req.body.password || !req.body.email || !req.body.prenom || !req.body.nom) {
-        res.status(400).send({ message: "Veuillez remplir tous les champs" });
-        return;
+
+    if (!req.body.username || !req.body.password || !req.body.email || !req.body.prenom || !req.body.nom) {
+        return res.status(400).send({ message: "Veuillez remplir tous les champs" });
     }
 
-    if(!req.body.picture) {
+    if (!req.body.picture) {
         req.body.picture = "https://www.w3schools.com/howto/img_avatar.png";
     }
     const hashedPassword = bcrypt.hashSync(req.body.password, 24);
     Teacher.create({
-        email : req.body.email,
-        picture:  req.body.picture,
-        password : hashedPassword,
-        username : req.body.username,
-        prenom : req.body.prenom,
-        nom : req.body.nom
+            email: req.body.email,
+            picture: req.body.picture,
+            password: hashedPassword,
+            username: req.body.username,
+            prenom: req.body.prenom,
+            nom: req.body.nom
         },
-        function (err, teacher) {
+        function(err, teacher) {
             if (err) return res.status(500).send("There was a problem registering the student.")
             console.log("Création d'un teacher effectué :");
             console.log(teacher)
             let token = jwt.sign({ id: teacher._id }, config.secret, {
                 expiresIn: 86400 // expires in 24 hours
-                });
-                res.status(200).send({ auth: "teacher", token: token});
-    });
+            });
+            return res.status(200).send({ auth: "teacher", token: token });
+        });
 }
 
 
